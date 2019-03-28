f4f4d327f025fcdafebd6f5214953b872852739e
mazerong <chdmazr@126.com>
2019-03-28 19:14:45 +0800

    section1 test2

--- src/main/java/cn/school/thoughtworks/section1/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeB.java
@@ -1,10 +1,13 @@
 package cn.school.thoughtworks.section1;
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class PracticeB {
     List<String> collectSameElements(List<String> collection1, List<List<String>> collection2) {
-
-        return null;
+        List collectionA = new ArrayList<String> (collection1);
+        List collectionB = new ArrayList<String> (collection2.get(0));
+        collectionA.retainAll(collectionB);
+        return collectionA;
     }
 }
