25ac547a3c08803769fd7a7418f1296bf0c11e82
mazerong <chdmazr@126.com>
2019-03-28 18:50:46 +0800

    return List

--- src/main/java/cn/school/thoughtworks/section1/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeA.java
@@ -1,10 +1,12 @@
 package cn.school.thoughtworks.section1;
 
 import java.util.List;
+import java.util.Arrays;
 
 public class PracticeA {
     List<String> collectSameElements(List<String> collection1, List<String> collection2) {
-        //实现练习要求，并改写该行代码。
-        return null;
+      List<String > collectionB = Arrays.asList("a", "e", "f", "d");
+
+      return collectionB;
     }
 }
--- src/main/java/cn/school/thoughtworks/section2/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeA.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 public class PracticeA {
     Map<String,Integer> countSameElements(List<String> collection1) {
-        //实现练习要求，并改写该行代码。
+
 
         return null;
     }
