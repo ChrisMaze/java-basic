fce39dd2cf912ed3502e942b0e01ed51f2d55f67
mazerong <chdmazr@126.com>
2019-03-28 20:01:09 +0800

    Section2 TestA

--- src/main/java/cn/school/thoughtworks/section2/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeA.java
@@ -2,12 +2,12 @@ package cn.school.thoughtworks.section2;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Collections;
+import java.util.HashMap;
 
 
 public class PracticeA {
     Map<String,Integer> countSameElements(List<String> collection1) {
-        Map map = new HashMap();
+        Map<String , Integer> map = new HashMap();
         for (String temp : collection1) {
             Integer count = map.get(temp);
             map.put(temp,(count == null) ? 1 : count + 1);
