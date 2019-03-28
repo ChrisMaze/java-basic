322a0105f0eac570e47d0a3ccb35c0c823587094
mazerong <chdmazr@126.com>
2019-03-28 22:37:43 +0800

    Section3 TestC

--- src/main/java/cn/school/thoughtworks/section3/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeC.java
@@ -2,11 +2,22 @@ package cn.school.thoughtworks.section3;
 
 import java.util.List;
 import java.util.Map;
+import  java.util.HashMap;
 
 public class PracticeC {
     Map<String,Integer> createUpdatedCollection(List<String> collectionA, Map<String,List<String>> object) {
-
-
-        return null;
+        Map<String , Integer> map = new HashMap();
+        for (String temp : collectionA) {
+            Integer count = map.get(temp);
+            map.put(temp,(count == null) ? 1 : count + 1);
+        }
+        for (String temp : object.get("value")) {
+            if (map.containsKey(temp))
+            {
+                int div = map.get(temp)/3;
+                map.put(temp,map.get(temp)-div);
+            }
+        }
+        return map;
     }
 }
